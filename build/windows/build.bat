b7bc7d00398e440699fa500bd63d3f90c1456084
Jaroslav Imrich <jaroslav.imrich@gmail.com>
2015-03-08 15:24:58 +0100

    Windows build script improvements

--- build/windows/build.bat
+++ build/windows/build.bat
@@ -1,16 +1,23 @@
+@setlocal
+
+@rem Initialize build environment of Visual Studio 2012
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\vsvars32.bat"
 
+@rem Build for x86 platform
 msbuild pkcs11-mock.sln /p:Configuration=Release /p:Platform=Win32 /target:Clean || goto :error
 msbuild pkcs11-mock.sln /p:Configuration=Release /p:Platform=Win32 /target:Build || goto :error
 copy .\Win32\Release\pkcs11-mock-x86.dll . || goto :error
 
+@rem Build for x64 platform
 msbuild pkcs11-mock.sln /p:Configuration=Release /p:Platform=x64 /target:Clean || goto :error
 msbuild pkcs11-mock.sln /p:Configuration=Release /p:Platform=x64 /target:Build || goto :error
 copy .\x64\Release\pkcs11-mock-x64.dll . || goto :error
 
-@echo "BUILD SUCCEEDED !!!"
+@echo *** BUILD SUCCESSFUL ***
+@endlocal
 @exit /b %errorlevel%
 
 :error
-@echo "BUILD FAILED !!!"
+@echo *** BUILD FAILED ***
+@endlocal
 @exit /b %errorlevel%
