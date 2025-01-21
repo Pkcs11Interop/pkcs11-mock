53d42bea07d49bb09e6a169f439ae7580e8353c7
Jaroslav Imrich <jimrich@jimrich.sk>
2025-01-21 23:31:32 +0100

    Builds with Visual Studio 2022

--- build/windows/build.bat
+++ build/windows/build.bat
@@ -1,20 +1,20 @@
-@setlocal
-
-@rem Initialize build environment of Visual Studio 2015
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
-
 @rem Build for x86 platform
+@setlocal
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 msbuild pkcs11-mock.sln /p:Configuration=Release /p:Platform=Win32 /target:Clean || goto :error
 msbuild pkcs11-mock.sln /p:Configuration=Release /p:Platform=Win32 /target:Build || goto :error
 copy .\Win32\Release\pkcs11-mock-x86.dll . || goto :error
+@endlocal
 
 @rem Build for x64 platform
+@setlocal
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 msbuild pkcs11-mock.sln /p:Configuration=Release /p:Platform=x64 /target:Clean || goto :error
 msbuild pkcs11-mock.sln /p:Configuration=Release /p:Platform=x64 /target:Build || goto :error
 copy .\x64\Release\pkcs11-mock-x64.dll . || goto :error
+@endlocal
 
 @echo *** BUILD SUCCESSFUL ***
-@endlocal
 @exit /b %errorlevel%
 
 :error
